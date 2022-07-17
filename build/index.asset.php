541b104b8ce1bf050810184af7a0e6a1d3774123
brent <brent@n3f.com>
2022-07-16 19:27:12 -0700

    Fix counter reset CSS.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('markdeep', 'react', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '555c43275b2ac5516694');
+<?php return array('dependencies' => array('markdeep', 'react', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '7cb8beec9b548e69ab1a');
